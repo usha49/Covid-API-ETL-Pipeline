1f929bb063ba00a4a5f268be5a05a967372fb6a3
Usha <usu.cha14@gmail.com>
2025-11-20 17:17:32 +0545

    table creation

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,2 +1,24 @@
 -- creating the database called covid_pipeline 
 create database Covid_Pipeline;
+
+--creaing dimension tables for the countries
+create table dim_countries (
+ country_id serial primary key,
+ country_name varchar(100),
+ iso_code varchar(10),
+ population BIGINT
+)
+
+--now creating fact table for the daily stats
+create table fact_covid_daily (
+f_id serial primary key,
+f_date date,
+country_id int references dim_countries(country_id),
+cases int,
+death int,
+recovered int,
+tests int
+);
+
+select * from dim_countries;
+select * from fact_covid_daily;
